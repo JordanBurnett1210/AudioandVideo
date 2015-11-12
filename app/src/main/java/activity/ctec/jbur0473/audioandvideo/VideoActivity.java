0bc302326a0271551ead3078a9103b73bc0938d5
JordanBurnett1210 <jabur1210@gmail.com>
2015-11-12 08:28:23 -0700

    finished

--- app/src/main/java/activity/ctec/jbur0473/audioandvideo/VideoActivity.java
+++ app/src/main/java/activity/ctec/jbur0473/audioandvideo/VideoActivity.java
@@ -4,13 +4,51 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.*;
+import android.net.Uri;
+import android.view.View;
 
-public class VideoActivity extends AppCompatActivity {
+public class VideoActivity extends AppCompatActivity
+{
+    private VideoView myPlayer;
+    private Button returnButton;
+    private MediaController myVideoController;
+    private Uri videoLocation;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        myPlayer = (VideoView) findViewById(R.id.videoView);
+        returnButton = (Button) findViewById(R.id.homeButton);
+
+        videoLocation = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.heyheman);
+        myVideoController = new MediaController(this);
+        //Prepare the video
+        setupMedia();
+        setupListeners();
+    }
+
+    private void setupMedia()
+    {
+        myPlayer.setMediaController(myVideoController);
+        myPlayer.setVideoURI(videoLocation);
+    }
+
+    private void setupListeners()
+    {
+        returnButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View currentView)
+            {
+                Intent returnIntent = new Intent();
+                setResult(RESULT_OK, returnIntent);
+                finish();
+            }
+        });
     }
 
     @Override
